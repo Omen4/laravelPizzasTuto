0511d576bc6a37d63bfd9718b85462b5162bd67d
Omen4 <florian.grosdidier@gmail.com>
2021-09-09 19:18:48 +0200

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/pizzas', function () {
+//    return view('pizzas');  //returning a view
+//    return "pizzas!";  //returning a string
+//    return ['name' => 'veg pizzas', 'base'=> 'classic']; //return an array (transformed into json through laravel)
+    $pizza = [
+        'type' => 'hawaiian',
+        'base' => 'garlic',
+        'price' => 10
+    ];
+
+    //get data from db
+    return view('pizzas', $pizza);  //returning a view with passed datas view('nameOfView', ['key' => ])
+});
+
